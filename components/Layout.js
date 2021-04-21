4c116774502498f0cdd0559216b9db9dde6416a1
Robert Reinert <robert.reinert@gmail.com>
2021-04-20 18:16:09 -0700

    width fix

--- components/Layout.js
+++ components/Layout.js
@@ -9,7 +9,7 @@ export default function Layout({ cats }) {
   const [attributes, setAttributes] = useState([]);
   const [name, setName] = useState({ target: { value: "" } });
   return (
-    <div>
+    <div className="w-full">
       <Searchbar
         handleBreedChange={setBreeds}
         handleColorChange={setColors}
