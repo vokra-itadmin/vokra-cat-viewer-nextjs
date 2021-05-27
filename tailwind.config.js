5c3a07523552da50560e393e43b341be03ee0096
Robert Reinert <robert.reinert@gmail.com>
2021-05-27 15:08:23 -0700

    Bonded attribute is Vokra purple

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "vokra-light": "#bd6cda",
+        "vokra-dark": "#986cda",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
