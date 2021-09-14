cb8246718f01fe8c061ce6c53ca1e63c17d69e58
Robert Reinert <robert.reinert@gmail.com>
2021-09-14 13:44:22 -0700

    cat slug fix

--- pages/cat/[catId].js
+++ pages/cat/[catId].js
@@ -1,15 +1,15 @@
-import { useRouter } from 'next/router';
-import { fetchCats, sanitizeCats } from '../../lib/api';
-import CatDetails from '../../components/CatDetails';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import { useRouter } from "next/router";
+import { fetchCats, sanitizeCats } from "../../lib/api";
+import CatDetails from "../../components/CatDetails";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 export async function getStaticPaths() {
-  const dirtyCats = await fetchCats();
-  const cats = await sanitizeCats(dirtyCats.animals);
+  const dirtyCats = await fetchCats(FETCH_URL);
+  const cats = await sanitizeCats(dirtyCats);
   return {
-    paths: cats.map(i => ({ params: { catId: i.ID } })),
-    fallback: 'blocking',
+    paths: cats.map((i) => ({ params: { catId: i.ID } })),
+    fallback: "blocking",
   };
 }
 
@@ -32,9 +32,9 @@ const CatPage = ({ cats }) => {
     <div className="w-full flex flex-col items-center">
       <Header />
       <div className="w-full flex justify-center bg-vokra-gray">
-        <div className="" style={{ width: '1200px' }}>
+        <div className="" style={{ width: "1200px" }}>
           <CatDetails
-            cat={cats.find(i => i.ID === catId)}
+            cat={cats.find((i) => i.ID === catId)}
             cats={cats}
             position="static"
             url
