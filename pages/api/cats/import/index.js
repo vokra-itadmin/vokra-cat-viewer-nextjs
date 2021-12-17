93d6306094af274c813887da65b812a476bf29bd
Robert Reinert <robert.reinert@gmail.com>
2021-12-17 14:33:32 -0800

    Added authentication to endpoints

--- pages/api/cats/import/index.js
+++ pages/api/cats/import/index.js
@@ -9,98 +9,112 @@ const parseInternalIdResp = (resp) => {
 };
 
 export default async function handler(req, res) {
-  const { since } = req.body;
-  const startTime = Math.floor(Date.now() / 1000);
-  const cats = await fetchCats(FETCH_URL, "", since);
-  let promises = [];
-  let errors = [];
-  let found = [];
-  let successes = 0;
-  let count = 0;
+  if (!req.headers.action_key) {
+    res.status(401).json("Access key required");
+  } else {
+    if (req.headers.action_key === process.env.APP_KEY) {
+      const { since } = req.body;
+      const startTime = Math.floor(Date.now() / 1000);
+      const cats = await fetchCats(FETCH_URL, "", since);
+      let promises = [];
+      let errors = [];
+      let found = [];
+      let successes = 0;
+      let count = 0;
 
-  for (let cat of cats) {
-    promises.push(findCat(cat["Internal-ID"]));
-    if (promises.length === 100 || promises.length === cats.length - count) {
-      const resp = await Promise.all(promises).catch((err) => {
-        errors.push({
-          type: "gql",
-          cat: cat.InternalID,
-          content: JSON.stringify(err),
-        });
-        console.error(err);
-      });
+      for (let cat of cats) {
+        promises.push(findCat(cat["Internal-ID"]));
+        if (
+          promises.length === 100 ||
+          promises.length === cats.length - count
+        ) {
+          const resp = await Promise.all(promises).catch((err) => {
+            errors.push({
+              type: "gql",
+              cat: cat["Internal-ID"],
+              content: JSON.stringify(err),
+            });
+            console.error(err);
+          });
 
-      if (resp) {
-        for (let element of resp) {
-          if (!element.findCatByInternalId) {
+          if (resp) {
+            for (let element of resp) {
+              if (!element.findCatByInternalId) {
+                errors.push({
+                  type: "gql",
+                  cat: cat.InternalID,
+                  content: "Unknown error during findCat",
+                });
+              }
+            }
+          }
+
+          found = found.concat(parseInternalIdResp(resp));
+          promises = [];
+          count += 100;
+        }
+      }
+      count = 0;
+      for (let cat of cats) {
+        delete Object.assign(cat, { ["InternalID"]: cat["Internal-ID"] })[
+          "Internal-ID"
+        ];
+        if (cat.Attributes) {
+          const fixedAttributes = cat.Attributes.map((element) => {
+            return {
+              InternalID: element["Internal-ID"],
+              AttributeName: element.AttributeName,
+              Publish: element.Publish,
+            };
+          });
+          cat.Attributes = fixedAttributes;
+        }
+        if (found.includes(cat.InternalID)) {
+          promises.push(updateCat(cat.InternalID, cat));
+        } else {
+          promises.push(createCat(cat));
+        }
+        if (
+          promises.length === 100 ||
+          promises.length === cats.length - count
+        ) {
+          const resp = await Promise.all(promises).catch((err) => {
             errors.push({
               type: "gql",
               cat: cat.InternalID,
-              content: "Unknown error during findCat",
+              content: JSON.stringify(err),
             });
+            console.error(err);
+          });
+
+          if (resp) {
+            for (let element of resp) {
+              if (element.updateCatByInternalId) {
+                successes++;
+              } else {
+                errors.push({
+                  type: "gql",
+                  cat: cat.InternalID,
+                  content: "Unknown error during cat create/update",
+                });
+              }
+            }
           }
+
+          promises = [];
+          count += 100;
         }
       }
-
-      found = found.concat(parseInternalIdResp(resp));
-      promises = [];
-      count += 100;
-    }
-  }
-  count = 0;
-  for (let cat of cats) {
-    delete Object.assign(cat, { ["InternalID"]: cat["Internal-ID"] })[
-      "Internal-ID"
-    ];
-    if (cat.Attributes) {
-      const fixedAttributes = cat.Attributes.map((element) => {
-        return {
-          InternalID: element["Internal-ID"],
-          AttributeName: element.AttributeName,
-          Publish: element.Publish,
-        };
+      res.status(200).json({
+        since: since,
+        startTime: startTime,
+        endTime: Math.floor(Date.now() / 1000),
+        tries: cats.length,
+        successes: successes,
+        errors: errors,
       });
-      cat.Attributes = fixedAttributes;
-    }
-    if (found.includes(cat.InternalID)) {
-      promises.push(updateCat(cat.InternalID, cat));
     } else {
-      promises.push(createCat(cat));
-    }
-    if (promises.length === 100 || promises.length === cats.length - count) {
-      const resp = await Promise.all(promises).catch((err) => {
-        errors.push({
-          type: "gql",
-          cat: cat.InternalID,
-          content: JSON.stringify(err),
-        });
-        console.error(err);
-      });
-
-      if (resp) {
-        for (let element of resp) {
-          if (element.updateCatByInternalId) {
-            successes++;
-          } else {
-            errors.push({
-              type: "gql",
-              cat: cat.InternalID,
-              content: "Unknown error during cat create/update",
-            });
-          }
-        }
-      }
-
-      promises = [];
-      count += 100;
+      res.status(401);
     }
   }
-  res.status(200).json({
-    since: since,
-    startTime: startTime,
-    endTime: Math.floor(Date.now() / 1000),
-    tries: cats.length,
-    successes: successes,
-    errors: errors,
-  });
 }
