377979f33154ec9caaff1baf1c5a26568fc55791
Robert Reinert <robert.reinert@gmail.com>
2022-02-07 12:26:29 -0800

    Merged api changes from dgraph-int

--- config/api.js
+++ config/api.js
@@ -1,3 +1,11 @@
-const FETCH_URL = 'https://www.shelterluv.com/api/v1/animals';
+const CAT_FETCH_URL = "https://www.shelterluv.com/api/v1/animals";
+const PEOPLE_FETCH_URL = "https://www.shelterluv.com/api/v1/people";
+const EVENTS_FETCH_URL = "https://www.shelterluv.com/api/v1/events";
+const VACCINES_FETCH_URL = "https://www.shelterluv.com/api/v1/vaccinces";
 
-export default FETCH_URL;
+export {
+  CAT_FETCH_URL,
+  PEOPLE_FETCH_URL,
+  EVENTS_FETCH_URL,
+  VACCINES_FETCH_URL,
+};
