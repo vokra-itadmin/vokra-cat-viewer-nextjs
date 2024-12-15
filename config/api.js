d4a83f3864eade153ca79bae14c2c546d1c2e633
vokra-itadmin <99102929+vokra-itadmin@users.noreply.github.com>
2024-12-15 10:29:11 -0800

    Updating ShelterLuv URL for new site.
    ShelterLuv updated their website and now use a slightly different URL.

--- config/api.js
+++ config/api.js
@@ -1,7 +1,7 @@
-const CAT_FETCH_URL = "https://www.shelterluv.com/api/v1/animals";
-const PEOPLE_FETCH_URL = "https://www.shelterluv.com/api/v1/people";
-const EVENTS_FETCH_URL = "https://www.shelterluv.com/api/v1/events";
-const VACCINES_FETCH_URL = "https://www.shelterluv.com/api/v1/vaccinces";
+const CAT_FETCH_URL = "https://new.shelterluv.com/api/v1/animals";
+const PEOPLE_FETCH_URL = "https://new.shelterluv.com/api/v1/people";
+const EVENTS_FETCH_URL = "https://new.shelterluv.com/api/v1/events";
+const VACCINES_FETCH_URL = "https://new.shelterluv.com/api/v1/vaccinces";
 
 export {
   CAT_FETCH_URL,
